9854a035cfcec7a486092d09a0aced40fcfca021
patrickgordon <pgord1@gmail.com>
2020-05-26 09:27:15 +1000

    remove unnecessary console logs

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,11 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Background got a message!");
-
   if (message === "highlight_excluded") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       var [activeTab] = tabs;
-
-      console.log(message);
 
       chrome.tabs.sendMessage(activeTab.id, message);
 
