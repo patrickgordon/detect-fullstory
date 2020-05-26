e21481dd560fb28346044b1a6aa4866baf9d63cb
patrickgordon <pgord1@gmail.com>
2020-05-26 10:50:47 +1000

    Lessen the perms

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,13 +1,20 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message === "highlight_excluded") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      var [activeTab] = tabs;
+    chrome.tabs.executeScript(
+      {
+        file: "js/content.js",
+      },
+      () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          var [activeTab] = tabs;
 
-      chrome.tabs.sendMessage(activeTab.id, message);
+          chrome.tabs.sendMessage(activeTab.id, message);
 
-      sendResponse({});
-      return true;
-    });
+          sendResponse({});
+          return true;
+        });
+      }
+    );
   }
 
   sendResponse({});
